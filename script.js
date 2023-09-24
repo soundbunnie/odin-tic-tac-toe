c3ba0cf1abef7da33d8a48abd9e097181bd31499
Aria Elkaim <ariari.elk@gmail.com>
2023-09-23 19:34:19 -0500

    Clean up code and some misc fixes

--- script.js
+++ script.js
@@ -1,224 +1,146 @@
-    /*
-    Functions: 
-    gameBoard() will build the gameboard by creating an array based on a number of columns and rows (3x3)
-        * This function will be an IIFE
-    gameController() will handle the game state, changing turns, check for win states, and playing the round
-    updateGameBoard() will update the gameboard with new information as the turns are played
-    gameBoard()
-        Create private variables storing the amount of rows and columns
-        Initialize array containing the game board
-        Create a nested loop that begins by iterating over the arrays' index of the current row, making sure that it's empty before continuing
-        Next, the loop will iterate over the amount of columns, pushing a cell to corresponding row in the array for each column before moving on to the ne"x"t row
+        var cells = document.getElementsByClassName("cell");
+        var activePlayerMessage = document.getElementById("active-player");
+        var winMessage = document.getElementById("win-message")
 
-    */
-    var cells = document.getElementsByClassName("cell");
-    var activePlayerMessage = document.getElementById("active-player");
-    var winMessage = document.getElementById("win-message")
+        var isWin = false;
 
-    document.getElementById("reset-button").addEventListener("click", function () {
-        resetBoard();
-    });    
+        document.getElementById("reset-button").addEventListener("click", function () {
+            gameController.resetBoard();
+        });    
 
-    var gameBoard = (function (){
-        var board = [];
-        const rows = 3;
-        const columns = 3;
+        var gameBoard = (function (){
+            var board = [];
+            const rows = 3;
+            const columns = 3;
 
-        for(let i = 0; i < rows; i++){
-            board[i] = [];
-            for (let j = 0; j < columns; j++){
-                board[i].push(0);
+            for (let i = 0; i < rows; i++) {
+                board[i] = new Array(columns).fill(0);
             }
-        }
-        return {board, rows, columns};
-    })();
+            return {board, rows, columns};
+        })();
 
-    const Player = (name, token) => {
-        const getName = () => name;
-        const getToken = () => token;
+        const Player = (name, token) => {
+            const getName = () => name;
+            const getToken = () => token;
 
-        const placeToken = (row, column, token) => {
-            if (row >= 0 && row < gameBoard.rows && column >= 0 && column < gameBoard.columns) {
+            function placeToken(row, column) {
                 if (gameBoard.board[row][column] === 0) {
                     gameBoard.board[row][column] = token;
                     gameController.switchTurn();
                 } else {
                     console.log("Invalid placement");
                 }
-            } else {
-                console.log("Invalid row or column values.");
             }
+            return{getName, getToken, placeToken};
         }
-        return{getName, getToken, placeToken};
-    }
 
-    var gameController = (function (){
-        const player1 = Player("Player One", 1);
-        const player2 = Player("Player Two", 2);
-        const _players = [player1, player2];
+        var gameController = (function (){
+            const player1 = Player("Player One", 1);
+            const player2 = Player("Player Two", 2);
+            const _players = [player1, player2];
 
-        let activePlayer = _players[0];
+            var activePlayer = player1;
 
-        var handleTurn = (function (desiredRow, desiredColumn){
-            console.log(`${activePlayer.getName()}'s turn.`)
-            console.log(gameBoard.board);
-            activePlayer.placeToken(desiredRow, desiredColumn, activePlayer.getToken());
-        })
+            var handleTurn = (function (desiredRow, desiredColumn){
+                if (!isWin){
+                    activePlayer.placeToken(desiredRow, desiredColumn, activePlayer.getToken());
+                }
+            })
 
-        var switchTurn = (function (){
-            activePlayer = activePlayer === _players[0] ? _players[1] : _players[0];
-            activePlayerMessage.textContent = `${activePlayer.getName()}'s turn.`;
-            printBoard();
-            checkWin();
-        })
+            var switchTurn = (function (){
+                activePlayer = activePlayer === player1 ? player2 : player1;
+                activePlayerMessage.textContent = `${activePlayer.getName()}'s turn.`;
+                printBoard();
+                checkWin();
+            })
 
-        var checkWin = (function (){
-            let board = gameBoard.board;
-            let rows = gameBoard.rows;
-            let columns = gameBoard.columns;
-            const allEqual = arr => arr.every(val => val === arr[0]); // Check if every value in the array matches the first element
-        
-            // Check rows for a win
-            for (let i = 0; i < rows; i++) {
-                if (allEqual(board[i])){ // Check if every all tokens in row is the same
-                    let sum = board[i].reduce((a, b) => a + b, 0); // Calculate the sum of the current row
-                    if (sum === 3) {
-                        winMessage.textContent = "Player One Wins";
+            var updateWin = (function (sum){
+                if (sum === 3){
+                    winMessage.textContent = "Player One Wins";
+                    isWin = true;
+                    return;
+                }
+                else if (sum === 6){
+                    winMessage.textContent = "Player Two Wins";
+                    isWin = true;
+                    return;
+                }
+            });
 
-                        return;
-                    } else if (sum === 6) {
-                        winMessage.textContent = "Player Two Wins";
-                        return;
+            var checkWin = (function (){
+                let board = gameBoard.board;
+                let rows = gameBoard.rows;
+                let columns = gameBoard.columns;
+                const allEqual = arr => arr.every(val => val === arr[0]); // Check if every value in the array matches the first element
+            
+                // Check rows for a win
+                for (let i = 0; i < rows; i++) {
+                    if (allEqual(board[i])){ // Check if every all tokens in row is the same
+                        let sum = board[i].reduce((a, b) => a + b, 0); // Calculate the sum of the current row
+                        updateWin(sum);
                     }
                 }
-            }
-        
-            // Check columns for a win
-            for (let j = 0; j < columns; j++) { // Loop through each column, represented by j
-                let verticalSum = 0;
-                let verticalBoard = [];
-                for (let i = 0; i < rows; i++) {
-                    verticalBoard.push(board[i][j]);
-                    if (allEqual(verticalBoard)){
-                        verticalSum = verticalBoard.reduce((a, b) => a + b, 0);
-                        if (verticalSum === 3) {
-                            winMessage.textContent = "Player One Wins";
-                            return;
-                        } else if (verticalSum === 6) {
-                            winMessage.textContent = "Player Two Wins";
-                            return;
+            
+                // Check columns for a win
+                for (let j = 0; j < columns; j++) { // Loop through each column, represented by j
+                    let verticalSum = 0;
+                    let verticalBoard = [];
+                    for (let i = 0; i < rows; i++) {
+                        verticalBoard.push(board[i][j]);
+                        if (allEqual(verticalBoard)){
+                            verticalSum = verticalBoard.reduce((a, b) => a + b, 0);
+                            updateWin(verticalSum);
                         }
                     }
                 }
-            }
-        
-            // Check diagonal from top-left to bottom-right
-            let diagonalSum = 0;
-            let diagonalBoard = [];
-            for (let i = 0; i < rows; i++) {
-                diagonalBoard.push(board[i][i]);
-                if (allEqual(diagonalBoard)){
-                    diagonalSum = diagonalBoard.reduce((a, b) => a + b, 0);
-                    if (diagonalSum === 3) {
-                        winMessage.textContent = "Player One Wins";
-                        return;
-                    } else if (diagonalSum === 6) {
-                        winMessage.textContent = "Player Two Wins";
-                        return;
+            
+                // Check diagonal from top-left to bottom-right
+                let diagonalSum = 0;
+                let diagonalBoard = [];
+                for (let i = 0; i < rows; i++) {
+                    diagonalBoard.push(board[i][i]);
+                    if (allEqual(diagonalBoard)){
+                        diagonalSum = diagonalBoard.reduce((a, b) => a + b, 0);
+                        updateWin(diagonalSum);
                     }
                 }
-            }
-        
-            // Check diagonal from top-right to bottom-left
-            diagonalSum = 0;
-            diagonalBoard = [];
-            for (let i = 0; i < rows; i++) {
-                diagonalBoard.push(board[i][rows - 1 - i])
-                if (allEqual(diagonalBoard)){
-                    diagonalSum = diagonalBoard.reduce((a, b) => a + b, 0);
-                    if (diagonalSum === 3) {
-                        winMessage.textContent = "Player One Wins";
-                    } else if (diagonalSum === 6) {
-                        winMessage.textContent = "Player Two Wins";
+            
+                // Check diagonal from top-right to bottom-left
+                diagonalSum = 0;
+                diagonalBoard = [];
+                for (let i = 0; i < rows; i++) {
+                    diagonalBoard.push(board[i][rows - 1 - i])
+                    if (allEqual(diagonalBoard)){
+                        diagonalSum = diagonalBoard.reduce((a, b) => a + b, 0);
+                        updateWin(diagonalSum);
                     }
                 }
-            }
-        });
+            });
 
-        return{_players, player1, player2, activePlayer, switchTurn, handleTurn};
-    })();
-
-    var printBoard = (function (){
-        for (let i = 0; i < cells.length; i++){ // Reset board
-            if (cells[i].textContent.trim() !== ''){
-                cells[i].textContent = '';
-            }
-        }
-        for (let i = 0; i < cells.length; i++){
-            let content;
-            if (i <= 2){
-                switch(gameBoard.board[0][i]){
-                    case 0:
-                        content = document.createTextNode("");
-                        cells[i].appendChild(content);
-                        break;
-                    case 1:
-                        content = document.createTextNode("X");
-                        cells[i].append(content);
-                        break;
-                    case 2:
-                        content = document.createTextNode("O");
-                        cells[i].append(content);
-                        break; 
+            var printBoard = (function () {
+                for (let i = 0; i < cells.length; i++) {
+                    const rowIndex = Math.floor(i / 3); // Divide by 3 because it's a 3x3 grid (6, 7, 8 are all 2 when floored, so they're the 2nd row)
+                    const colIndex = i % 3; 
+                    // Use modulo because the column index has to wrap around when reaching the end of a row, which is why I get 
+                    // the remainder of i after dividing it by the number of columns
+                    const content = gameBoard.board[rowIndex][colIndex];
+                    cells[i].textContent = content === 1 ? 'X' : content === 2 ? 'O' : '';
                 }
-            }
-            else if (i <= 5 && i > 2){
-                switch(gameBoard.board[1][i - 3]){
-                    case 0:
-                        content = document.createTextNode("");
-                        cells[i].appendChild(content);
-                        break;
-                    case 1:
-                        content = document.createTextNode("X");
-                        cells[i].append(content);
-                        break;
-                    case 2:
-                        content = document.createTextNode("O");
-                        cells[i].append(content);
-                        break; 
+            });        
+    
+            var resetBoard = (function () {
+                let board = gameBoard.board;
+                isWin = false;
+                for (let i = 0; i < gameBoard.rows; i++) {
+                    for (let j = 0; j < gameBoard.columns; j++) {
+                        board[i][j] = 0;
+                    }
                 }
-            }
-            else if (i <= 8 && i > 5){
-                switch(gameBoard.board[2][i - 6]){
-                    case 0:
-                        content = document.createTextNode("");
-                        cells[i].appendChild(content);
-                        break;
-                    case 1:
-                        content = document.createTextNode("X");
-                        cells[i].append(content);
-                        break;
-                    case 2:
-                        content = document.createTextNode("O");
-                        cells[i].append(content);
-                        break; 
-                }
-            }
-        }
-        {return printBoard};
-    });
-
-    var resetBoard = function () {
-        let board = gameBoard.board;
-        for (let i = 0; i < gameBoard.rows; i++) {
-            for (let j = 0; j < gameBoard.columns; j++) {
-                board[i][j] = 0;
-            }
-        }
-        winMessage.textContent = "";
-        if (gameController.activePlayer === gameController._players[0]){
-            gameController.switchTurn();
-        }
-        printBoard();
-        return board; // Return the updated board
-    };
+                activePlayer = player1;
+                activePlayerMessage.textContent = `${gameController.activePlayer.getName()}'s turn.`; // Update active player message
+                winMessage.textContent = "";
+                printBoard();
+            });
+            return{_players, player1, player2, activePlayer, switchTurn, handleTurn, resetBoard, printBoard};
+        })();
+        
